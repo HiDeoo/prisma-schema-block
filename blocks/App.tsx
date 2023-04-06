31b672fcca5eb04d082f7777c38e7f664b3a94fe
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-04-06 10:18:44 +0200

    feat: render basic reactflow

--- blocks/App.tsx
+++ blocks/App.tsx
@@ -1,6 +1,15 @@
+import 'reactflow/dist/style.css'
+
 import { type FileBlockProps } from '@githubnext/blocks'
+import { ReactFlow } from 'reactflow'
 
 import { getDefinitionsFromSchema } from './libs/prisma'
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+]
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
 
 // TODO(HiDeoo) error boundary
 // TODO(HiDeoo) nis
@@ -9,5 +18,9 @@ export default function App({ content }: FileBlockProps) {
 
   console.error('🚨 [App.tsx:11] definitions:', definitions)
 
-  return <div>Hello</div>
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    </div>
+  )
 }
